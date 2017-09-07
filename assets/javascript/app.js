6e058ad2879b42dfe65cc0c052784614af929e3b
raj594 <rajaynes@gmail.com>
2017-09-07 14:57:15 -0500

    change to https request

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@
       function displayMovieInfo() {
 
         var gif = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=" + apiKey + "&limit=10&rating=g&rating=pg";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=" + apiKey + "&limit=10&rating=g&rating=pg";
         console.log(queryURL)
         $('#gif-view').empty();
         // Creating an AJAX call for the specific gif button being clicked
